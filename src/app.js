9f7ffa9d0092e708395397d466f7a5f08e0ad4f2
JulioV10 <julio.vargas11@hotmail.com>
2023-03-07 17:15:23 +0000

    random cards

--- src/app.js
+++ src/app.js
@@ -9,3 +9,36 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let icons = ["♦", "♥", "♠", "♣"];
+let cardNumbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A"
+];
+let randomNumber = function(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+let changeCardIcon = document.querySelectorAll(".symbol");
+
+let cardIcon = randomNumber(0, 4);
+
+let icon = icons[cardIcon];
+
+for (let i = 0; i < changeCardIcon.length; i++) {
+  changeCardIcon[i].textContent = icon;
+  changeCardIcon[i].style.color = cardIcon < 2 ? "red" : "black";
+}
+
+document.getElementById("number").textContent =
+  cardNumbers[randomNumber(0, 13)];
